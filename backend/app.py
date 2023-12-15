e491496b144776d1e13e0be962d2d0ae4db07861
Chinmay Kulkarni <kulkarnichinmay65@gmail.com>
2023-12-16 00:15:34 +0530

    Adding a fail safe for wrong endpoints

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Please use the /prompt endpoint to POST data.', 200
+    return '''
+    <html>
+        <body style="background-color: black; color: white; font-family: monospace;">
+            Please use the /prompt endpoint to POST data.
+        </body>
+    </html>
+    ''', 200
+
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def catch_all(path):
+    if request.method == 'POST':
+        return jsonify({'error': 'This endpoint "{}" does not exist'.format(path)}), 404
+    else:
+        return '''
+        <html>
+            <body style="background-color: black; color: white; font-family: monospace;">
+                This page "{}" does not exist
+            </body>
+        </html>
+        '''.format(path), 404
 
 @app.route('/prompt', methods=['POST'])
 def handle_prompt():
